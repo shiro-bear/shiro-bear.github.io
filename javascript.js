8b1cd6717df0fdbd631f8dc1b644b170ebb9bf96
shiro-bear <122776793+shiro-bear@users.noreply.github.com>
2023-01-16 21:08:19 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,18 @@
-<script type="text/javascript">
-	function message() {
-	  document.getElementById("view_time").innerHTML = getNow();
-	}
+function message() {
+  document.getElementById("view_time").innerHTML = getNow();
+}
 
-	function getNow() {
-		var now = new Date();
-		var year = now.getFullYear();
-		var mon = now.getMonth()+1; //１を足すこと
-		var day = now.getDate();
-		var hour = now.getHours();
-		var min = now.getMinutes();
-		var sec = now.getSeconds();
-
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var sec = now.getSeconds();
 		//出力用
-		var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
-		return s;
-	}
-	const swiper = new Swiper(".swiper",{});
+	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
+	return s;
+}
+const swiper = new Swiper(".swiper",{});
 </script>
